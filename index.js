5483bf60f2948d5b5fb12d85e4615509465c42d1
Naim Ahmad <naimahmad201103@gmail.com>
2023-11-12 00:20:29 +0600

    project scafolding initiate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import "dotenv/config";
 import cors from "cors";
-import connectToDatabase from "./src/dbconfigure/config.js";
+import "dotenv/config";
+import express from "express";
+import connectToDatabase from "./config/db.config.js";
 const port = process.env.PORT || 3000;
 
 // intialized a exprss app
@@ -14,9 +14,26 @@ app.use(express.json());
 // database connection
 connectToDatabase();
 
+
+// default error handling middleware
+
+app.use((req, res, next)=>{
+  const error = new Error("Resource not found!")
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next)=>{
+  console.log(error);
+  if(error.status){
+    return res.status(error.status).json({message: error.message})
+  }
+  res.status(500).json({message: "something went wrong"})
+})
+
 // home route
-app.get("/", (req, res) => {
-  res.send("Product Management server is Running :)");
+app.get("/", (_req, res, next) => {
+  res.status(200).json({message: "Product Management server is Running :)"});
 });
 
 // listen app
